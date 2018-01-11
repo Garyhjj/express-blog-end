f2fd5b8d013248d6ab24e78942852c23b8e15ed6
Garyhjj <379751452@qq.com>
2018-01-11 14:13:56 +0800

    improve

--- index.js
+++ index.js
@@ -11,9 +11,20 @@ var routes = require('./routes');
 
 var app = express();
 
-app.use(cors());
+// app.use(cors());
 //设置跨域访问
-
+app.all('*',function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  
+    if (req.method == 'OPTIONS') {
+      res.send(200); /让options请求快速返回/
+    }
+    else {
+      next();
+    }
+  });
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
